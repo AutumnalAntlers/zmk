0c097c97adcf116b1a68e9ef32a6b2acf29f4f05
antlers <antlers@illucid.net>
2023-06-10 19:25:20 +0000

    what if we just dont

--- app/src/custom/process_autocorrect.c
+++ app/src/custom/process_autocorrect.c
@@ -7,7 +7,7 @@
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/event_manager.h>
-#include <zmk/events/autocorrect_state_changed.h>
+// #include <zmk/events/autocorrect_state_changed.h>
 #include <zmk/events/keycode_state_changed.h>
 
 #include <dt-bindings/zmk/keys.h>
