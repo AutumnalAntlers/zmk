826ddff53db4226b7ec39841fb7e9f599ae22ab5
antlers <antlers@illucid.net>
2023-06-10 19:28:40 +0000

    fix include

--- app/src/custom/process_autocorrect.c
+++ app/src/custom/process_autocorrect.c
@@ -12,13 +12,13 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <dt-bindings/zmk/keys.h>
 
-#include <zmk/process_autocorrect.h>
+#include <zmk/custom/process_autocorrect.h>
 
 #if __has_include("autocorrect_data.h")
-#    include "autocorrect_data.h"
+#    include <zmk/custom/autocorrect_data.h>
 #else
 #    pragma message "Autocorrect is using the default library."
-#    include "autocorrect_data_default.h"
+#    include <zmk/custom/autocorrect_data_default.h>
 #endif
 
 #include <string.h>
