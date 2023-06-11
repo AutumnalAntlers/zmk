a1370f51140761be4e938a1a0b0f62a963f4a038
antlers <antlers@illucid.net>
2023-06-11 20:35:02 +0000

    int16_t

--- app/src/custom/make_autocorrection_data.py
+++ app/src/custom/make_autocorrection_data.py
@@ -302,7 +302,7 @@ def write_generated_code(autocorrections: List[Tuple[str, str]],
     f'\n#include <dt-bindings/zmk/keys.h>\n',
     f'\n#define AUTOCORRECTION_MIN_LENGTH {len(min_typo)}  // "{min_typo}"\n',
     f'#define AUTOCORRECTION_MAX_LENGTH {len(max_typo)}  // "{max_typo}"\n\n',
-    textwrap.fill('static const int autocorrection_data[%d] = {%s};' % (
+    textwrap.fill('static const int16_t autocorrection_data[%d] = {%s};' % (
       len(data), ', '.join(map(decode, data))), width=80, subsequent_indent='  '),
     '\n\n'])
 
