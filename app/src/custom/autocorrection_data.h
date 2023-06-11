9e6d4b93797f6673aa325b13f7e9ff521882cf7d
antlers <antlers@illucid.net>
2023-06-11 20:14:12 +0000

    lets try this

---
+++ app/src/custom/autocorrection_data.h
@@ -0,0 +1,131 @@
+// Generated code.
+
+// Autocorrection dictionary (70 entries):
+//   :guage     -> gauge
+//   :the:the:  -> the
+//   :thier     -> their
+//   :ture      -> true
+//   accomodate -> accommodate
+//   acommodate -> accommodate
+//   aparent    -> apparent
+//   aparrent   -> apparent
+//   apparant   -> apparent
+//   apparrent  -> apparent
+//   aquire     -> acquire
+//   becuase    -> because
+//   cauhgt     -> caught
+//   cheif      -> chief
+//   choosen    -> chosen
+//   cieling    -> ceiling
+//   collegue   -> colleague
+//   concensus  -> consensus
+//   contians   -> contains
+//   cosnt      -> const
+//   dervied    -> derived
+//   fales      -> false
+//   fasle      -> false
+//   fitler     -> filter
+//   flase      -> false
+//   foward     -> forward
+//   frequecy   -> frequency
+//   gaurantee  -> guarantee
+//   guaratee   -> guarantee
+//   heigth     -> height
+//   heirarchy  -> hierarchy
+//   inclued    -> include
+//   interator  -> iterator
+//   intput     -> input
+//   invliad    -> invalid
+//   lenght     -> length
+//   liasion    -> liaison
+//   libary     -> library
+//   listner    -> listener
+//   looses:    -> loses
+//   looup      -> lookup
+//   manefist   -> manifest
+//   namesapce  -> namespace
+//   namespcae  -> namespace
+//   occassion  -> occasion
+//   occured    -> occurred
+//   ouptut     -> output
+//   ouput      -> output
+//   overide    -> override
+//   postion    -> position
+//   priviledge -> privilege
+//   psuedo     -> pseudo
+//   recieve    -> receive
+//   refered    -> referred
+//   relevent   -> relevant
+//   repitition -> repetition
+//   retrun     -> return
+//   retun      -> return
+//   reuslt     -> result
+//   reutrn     -> return
+//   saftey     -> safety
+//   seperate   -> separate
+//   singed     -> signed
+//   stirng     -> string
+//   strign     -> string
+//   swithc     -> switch
+//   swtich     -> switch
+//   thresold   -> threshold
+//   udpate     -> update
+//   widht      -> width
+
+#include <dt-bindings/zmk/keys.h>
+
+#define AUTOCORRECTION_MIN_LENGTH 5  // ":ture"
+#define AUTOCORRECTION_MAX_LENGTH 10  // "accomodate"
+
+static const uint16_t autocorrection_data[1104] = {L, 43, 0, 6, 71, 0, 7, 81, 0,
+  8, 199, 0, 9, 240, 1, 10, 250, 1, 11, 26, 2, 17, N5, 2, 18, 190, 2, 19, 202,
+  2, 21, 212, 2, 22, 20, 3, 23, QUOT, 3, 28, 16, 4, 0, 72, N2, 0, 22, 60, 0, 0,
+  11, 23, SPACE, 8, 11, 23, SPACE, 0, 132, 0, 8, 22, 18, 18, 15, 0, 132, S, E,
+  S, 0, 11, 23, 12, 26, 22, 0, 129, C, H, 0, 68, 94, 0, 8, J, 0, 15, 174, 0, 21,
+  187, 0, 0, 12, 15, 25, 17, 12, 0, 131, 97, L, I, D, 0, 74, W, 0, 12, 129, 0,
+  21, 140, 0, 24, 165, 0, 0, 17, 12, 22, 0, 131, G, N, E, D, 0, 25, 21, 8, 7, 0,
+  131, I, V, E, D, 0, 72, 147, 0, 24, 156, 0, 0, 9, 8, 21, 0, 129, R, E, D, 0,
+  6, 6, 18, 0, 129, R, E, D, 0, 15, 6, 17, 12, 0, 129, D, E, 0, 18, 22, 8, 21,
+  11, 23, 0, 130, H, O, L, D, 0, 4, 26, 18, 9, 0, 131, R, W, 97, R, D, 0, 68,
+  233, 0, 6, 246, 0, 7, 4, 1, 8, 16, 1, 10, N4, 1, 15, 81, 1, 21, 90, 1, 22, U,
+  1, 23, 144, 1, 24, 215, 1, 25, 228, 1, 0, 6, 19, 22, 8, 16, 4, 17, 0, 130, 97,
+  C, E, 0, 19, 4, 22, 8, 16, 4, 17, 0, 131, P, 97, C, E, 0, 12, 21, 8, 25, 18,
+  0, 130, R, I, D, E, 0, 23, 0, 68, 25, 1, 17, 36, 1, 0, 21, 4, 24, 10, 0, 130,
+  N, T, E, E, 0, 4, 21, 24, 4, 10, 0, 135, U, 97, R, 97, N, T, E, E, 0, 68, 59,
+  1, 7, 69, 1, 0, 24, 10, SPACE, 0, 131, 97, U, G, E, 0, 8, 15, 12, 25, 12, 21,
+  19, 0, 130, G, E, 0, 22, 4, 9, 0, 130, L, S, E, 0, 76, 97, 1, 24, M, 1, 0, 24,
+  20, 4, 0, 132, C, Q, U, I, R, E, 0, 23, SPACE, 0, 130, R, U, E, 0, 4, 0, 79,
+  126, 1, 24, 134, 1, 0, 9, 0, 131, 97, L, S, E, 0, 6, 8, 5, 0, 131, 97, U, S,
+  E, 0, 4, 0, 71, 156, 1, 19, 193, 1, 21, 203, 1, 0, 18, 16, 0, 80, 166, 1, 18,
+  181, 1, 0, 18, 6, 4, 0, 135, C, O, M, M, O, D, 97, T, E, 0, 6, 6, 4, 0, 132,
+  M, O, D, 97, T, E, 0, 7, 24, 0, 132, P, D, 97, T, E, 0, 8, 19, 8, 22, 0, 132,
+  97, R, 97, T, E, 0, 10, 8, 15, 15, 18, 6, 0, 130, 97, G, U, E, 0, 8, 12, 6, 8,
+  21, 0, 131, E, I, V, E, 0, 12, 8, 11, 6, 0, 130, I, E, F, 0, 17, 0, 76, 3, 2,
+  21, 16, 2, 0, 15, 8, 12, 6, 0, 133, E, I, L, I, N, G, 0, 12, 23, 22, 0, 131,
+  R, I, N, G, 0, 70, 33, 2, 23, SPACE, 2, 0, 12, 23, 26, 22, 0, 131, I, T, C, H,
+  0, 10, 12, 8, 11, 0, 129, H, T, 0, 72, 69, 2, 10, 80, 2, 18, 89, 2, 21, 156,
+  2, 24, 167, 2, 0, 22, 18, 18, 11, 6, 0, 131, S, E, N, 0, 12, 21, 23, 22, 0,
+  129, N, G, 0, 12, 0, 86, B, 2, 23, 124, 2, 0, 68, I, 2, 22, R, 2, 0, 12, 15,
+  0, 131, I, S, O, N, 0, 4, 6, 6, 18, 0, 131, I, O, N, 0, 76, 131, 2, 22, 146,
+  2, 0, 23, 12, 19, 8, 21, 0, 134, E, T, I, T, I, O, N, 0, 18, 19, 0, 131, I, T,
+  I, O, N, 0, 23, 24, 8, 21, 0, 131, T, U, R, N, 0, 85, 174, 2, 23, 183, 2, 0,
+  23, 8, 21, 0, 130, U, R, N, 0, 8, 21, 0, 128, R, N, 0, 7, 8, 24, 22, 19, 0,
+  131, E, U, D, O, 0, 24, 18, 18, 15, 0, 129, K, U, P, 0, 72, 219, 2, 18, 3, 3,
+  0, 76, 229, 2, 15, 238, 2, 17, 248, 2, 0, 11, 23, SPACE, 0, 130, E, I, R, 0,
+  23, 12, 9, 0, 131, L, T, E, R, 0, 23, 22, 12, 15, 0, 130, E, N, E, R, 0, 23,
+  4, 21, 8, 23, 17, 12, 0, 135, T, E, R, 97, T, O, R, 0, 72, 30, 3, 17, 38, 3,
+  24, N3, 3, 0, 15, 4, 9, 0, 129, S, E, 0, 4, 12, 23, 17, 18, 6, 0, 131, 97, I,
+  N, S, 0, 22, 17, 8, 6, 17, 18, 6, 0, 133, S, E, N, S, U, S, 0, 74, 86, 3, 11,
+  96, 3, 15, V, 3, 17, 129, 3, 22, 218, 3, 24, 232, 3, 0, 11, 24, 4, 6, 0, 130,
+  G, H, T, 0, 71, G, 3, 10, N, 3, 0, 12, 26, 0, 129, T, H, 0, 17, 8, 15, 0, 129,
+  T, H, 0, 22, 24, 8, 21, 0, 131, S, U, L, T, 0, 68, 139, 3, 8, 150, 3, 22, 210,
+  3, 0, 21, 4, 19, 19, 4, 0, 130, E, N, T, 0, 85, 157, 3, 25, 200, 3, 0, 68,
+  164, 3, 21, 175, 3, 0, 19, 4, 0, 132, P, 97, R, E, N, T, 0, 4, 19, 0, 68, 185,
+  3, 19, 193, 3, 0, 133, P, 97, R, E, N, T, 0, 4, 0, 131, E, N, T, 0, 8, 15, 8,
+  21, 0, 130, 97, N, T, 0, 18, 6, 0, 130, N, S, T, 0, 12, 9, 8, 17, 4, 16, 0,
+  132, I, F, E, S, T, 0, 83, 239, 3, 23, 6, 4, 0, 87, 246, 3, 24, 254, 3, 0, 17,
+  12, 0, 131, P, U, T, 0, 18, 0, 130, T, P, U, T, 0, 19, 24, 18, 0, 131, T, P,
+  U, T, 0, 70, 29, 4, 8, 41, 4, 11, N3, 4, 21, 69, 4, 0, 8, 24, 20, 8, 21, 9, 0,
+  129, N, C, Y, 0, 23, 9, 4, 22, 0, 130, E, T, Y, 0, 6, 21, 4, 21, 12, 8, 11, 0,
+  135, I, E, R, 97, R, C, H, Y, 0, 4, 5, 12, 15, 0, 130, R, 97, R, Y, 0};
+
