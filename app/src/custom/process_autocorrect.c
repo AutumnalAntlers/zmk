f7d1035363837d2699b35b55ddb014470d5d9ca7
antlers <antlers@illucid.net>
2023-06-12 23:09:09 +0000

    much debug

--- app/src/custom/process_autocorrect.c
+++ app/src/custom/process_autocorrect.c
@@ -29,12 +29,13 @@ const uint32_t HIGH_BIT_MASK = 1073741823; // (2**32 >> 2) - 1
 static uint32_t typo_buffer[AUTOCORRECT_MAX_LENGTH] = {SPACE};
 static uint32_t typo_buffer_size                    = 1;
 
-int log_array(char name[], uint32_t array[], int length) {
+int log_array(int num, char name[], uint32_t array[], int length) {
   if (length == -1) {
     length = sizeof(array) / sizeof(array[0]);
   }
+  LOG_DBG("[ANT %02d] Log Array: %s", num, name)
   for (int i = 0; i < length; i++) {
-    LOG_DBG("[ANT] LOG_ARRAY %s %d/%d] %d [%c]", name, i + 1, length, array[i], (char) (array[i] + 61));
+    LOG_DBG("[ANT %02d %d/%d] %d [%c]", num, i + 1, length, array[i], (char) (array[i] + 61));
   }
 }
 
@@ -47,7 +48,7 @@ int autocorrect_event_listener(const zmk_event_t *eh) {
   if (ev) {
     // count only key up events
     if (!ev->state) {
-      LOG_DBG("[ANT-01] keycode: %d [%c]", ev->keycode, (char) (ev->keycode + 61));
+      LOG_DBG("[ANT 01] keycode: %d [%c]", ev->keycode, (char) (ev->keycode + 61));
       process_autocorrect(ev->keycode, eh);
     }
   }
@@ -58,152 +59,153 @@ bool process_autocorrect(uint32_t keycode, const zmk_event_t *record) {
   const struct zmk_keycode_state_changed *ev = as_zmk_keycode_state_changed(record);
   switch (keycode) {
     case A ... Z:
-      LOG_DBG("[ANT-02a]");
+      LOG_DBG("[ANT 02a]");
       // process normally
       break;
     case 4 ... 29:
-      LOG_DBG("[ANT-02b]");
+      LOG_DBG("[ANT 02b]");
       // process normally
       break;
     case N1 ... N0:
-      LOG_DBG("[ANT-03a]");
+      LOG_DBG("[ANT 03a]");
     case 30 ... 39:
-      LOG_DBG("[ANT-03b]");
+      LOG_DBG("[ANT 03b]");
     case TAB ... SEMICOLON:
-      LOG_DBG("[ANT-04a]");
+      LOG_DBG("[ANT 04a]");
     case 45 ... 51:
-      LOG_DBG("[ANT-04b]");
+      LOG_DBG("[ANT 04b]");
     case GRAVE ... SLASH:
-      LOG_DBG("[ANT-05a]");
+      LOG_DBG("[ANT 05a]");
       // Set a word boundary if space, period, digit, etc. is pressed.
       keycode = SPACE;
       break;
     case 53 ... 56:
-      LOG_DBG("[ANT-05b]");
+      LOG_DBG("[ANT 05b]");
       // Set a word boundary if space, period, digit, etc. is pressed.
       keycode = 44;
       break;
     case 44:
-      LOG_DBG("[ANT-05c]");
+      LOG_DBG("[ANT 05c]");
       // Set a word boundary if space, period, digit, etc. is pressed.
       keycode = 44;
       break;
     case ENTER:
-      LOG_DBG("[ANT-05.5a]");
+      LOG_DBG("[ANT 05.5a]");
       // Behave more conservatively for the enter key. Reset, so that enter
       // can't be used on a word ending.
       typo_buffer_size = 0;
       keycode          = 44;
       break;
     case 40:
-      LOG_DBG("[ANT-05.5b]");
+      LOG_DBG("[ANT 05.5b]");
       // Behave more conservatively for the enter key. Reset, so that enter
       // can't be used on a word ending.
       typo_buffer_size = 0;
       keycode          = 44;
       break;
     case BSPC:
-      LOG_DBG("[ANT-06a]");
+      LOG_DBG("[ANT 06a]");
       // Remove last character from the buffer.
       if (typo_buffer_size > 0) {
           --typo_buffer_size;
       }
       return true;
     case 42:
-      LOG_DBG("[ANT-06b]");
+      LOG_DBG("[ANT 06b]");
       // Remove last character from the buffer.
       if (typo_buffer_size > 0) {
           --typo_buffer_size;
       }
       return true;
     case DQT:
-      LOG_DBG("[ANT-07a]");
+      LOG_DBG("[ANT 07a]");
       // Treat " as a word boundary.
       keycode = 44;
       break;
     case 52: // XXX: uhm, that's a single quote
-      LOG_DBG("[ANT-07b]");
+      LOG_DBG("[ANT 07b]");
       // Treat " as a word boundary.
       keycode = 44;
       break;
     default:
-      LOG_DBG("[ANT-08]");
+      LOG_DBG("[ANT 08]");
       // Clear state if some other non-alpha key is pressed.
       typo_buffer_size = 0;
       return true;
   }
 
-  LOG_DBG("[ANT] typo_buffer_size: %d", typo_buffer_size);
-  log_array("TYPO_BUFFER", typo_buffer, typo_buffer_size);
+  LOG_DBG("[ANT 09 0/%d] typo_buffer_size: %d", typo_buffer_size + 1, typo_buffer_size);
+  log_array(9, "TYPO_BUFFER", typo_buffer, typo_buffer_size);
   // Rotate oldest character if buffer is full.
   if (typo_buffer_size >= AUTOCORRECT_MAX_LENGTH) {
-      LOG_DBG("[ANT-10] Rotating buffer");
-      memmove(typo_buffer, typo_buffer + 1, AUTOCORRECT_MAX_LENGTH - 1);
+      LOG_DBG("[ANT 10] Rotating buffer (shown pre-truncation)");
+      memmove(typo_buffer, typo_buffer + 1, (AUTOCORRECT_MAX_LENGTH * sizeof(typo_buffer[0])) - 1);
+      LOG_DBG("[ANT 10] typo_buffer_size: %d", typo_buffer_size);
+      log_array(10, "TYPO_BUFFER", typo_buffer, typo_buffer_size);
+      LOG_DBG("[ANT 10] Truncating buffer");
       typo_buffer_size = AUTOCORRECT_MAX_LENGTH - 1;
-      LOG_DBG("[ANT] typo_buffer_size: %d", typo_buffer_size);
-      log_array("TYPO_BUFFER", typo_buffer, typo_buffer_size);
   }
 
-  LOG_DBG("[ANT-11] Appending keycode to buffer");
+  LOG_DBG("[ANT 11] Appending keycode to buffer");
   // Append `keycode` to buffer.
   typo_buffer[typo_buffer_size++] = keycode;
+
   // Return if buffer is smaller than the shortest word.
   if (typo_buffer_size < AUTOCORRECT_MIN_LENGTH) {
-      LOG_DBG("[ANT-12] Returning early because buffer is shorter than any match");
+      LOG_DBG("[ANT 12] Returning early");
       return true;
   }
-  LOG_DBG("[ANT] typo_buffer_size: %d", typo_buffer_size);
-  log_array("TYPO_BUFFER", typo_buffer, typo_buffer_size);
+  LOG_DBG("[ANT 13] typo_buffer_size: %d", typo_buffer_size);
+  log_array(13, "TYPO_BUFFER", typo_buffer, typo_buffer_size);
 
   // Check for typo in buffer using a trie stored in `autocorrect_data`.
   int state = 0;
   uint32_t code  = autocorrect_data[state];
   for (uint32_t i = typo_buffer_size - 1; i >= 0; --i) {
-    LOG_DBG("[ANT-14 1/4] i: %d", i);
+    LOG_DBG("[ANT 14 1/4] i: %d", i);
     uint32_t const key_i = typo_buffer[i];
-    LOG_DBG("[ANT-14 2/4] code: %d [%c]", code, (char) (code + 61));
-    LOG_DBG("[ANT-14 3/4] key_i: %d [%c]", key_i, (char) (key_i + 61));
-    LOG_DBG("[ANT-14 4/4] state: %d", state);
+    LOG_DBG("[ANT 14 2/4] code: %d [%c]", code, (char) (code + 61));
+    LOG_DBG("[ANT 14 3/4] key_i: %d [%c]", key_i, (char) (key_i + 61));
+    LOG_DBG("[ANT 14 4/4] state: %d", state);
     if (code & (HIGH_BIT_MASK + 1)) { // Check for match in node with multiple children.
       code &= HIGH_BIT_MASK;
-      LOG_DBG("[ANT-15 1/3] code: %d", code);
+      LOG_DBG("[ANT 15 1/3] code: %d", code);
       for (; code != key_i; code = autocorrect_data[(state += 3)]) {
-        LOG_DBG("[ANT-15 2/3] code: %d", code);
+        LOG_DBG("[ANT 15 2/3] code: %d", code);
         if (!code) return true;
       }
-      LOG_DBG("[ANT-15 3/3] code: %d", code);
+      LOG_DBG("[ANT 15 3/3] code: %d", code);
       // Follow link to child node.
-      LOG_DBG("[ANT-16] pre-state: %d", state);
+      LOG_DBG("[ANT 16] pre-state: %d", state);
       state = (autocorrect_data[(state + 1)] | autocorrect_data[(state + 2)] << 8);
-      LOG_DBG("[ANT-17] post-state: %d", state);
+      LOG_DBG("[ANT 17] post-state: %d", state);
       // Check for match in node with single child.
     } else if (code != key_i) {
-      LOG_DBG("[ANT-18] No match");
+      LOG_DBG("[ANT 18] No match");
       return true;
     } else if (!(code = autocorrect_data[(++state)])) {
-      LOG_DBG("[ANT-19] pre-state: %d, code: %d, data: %d", state, code, autocorrect_data[state]);
+      LOG_DBG("[ANT 19] pre-state: %d, code: %d, data: %d", state, code, autocorrect_data[state]);
       ++state;
-      LOG_DBG("[ANT-19] post-state: %d, code: %d, data: %d", state, code, autocorrect_data[state]);
+      LOG_DBG("[ANT 19] post-state: %d, code: %d, data: %d", state, code, autocorrect_data[state]);
     }
 
     // Stop if `state` becomes an invalid index. This should not normally
     // happen, it is a safeguard in case of a bug, data corruption, etc.
     if (state >= (sizeof(autocorrect_data)/sizeof(autocorrect_data[0]))) {
-      LOG_DBG("[ANT-19.25] warning: state too big (%d)", state);
+      LOG_DBG("[ANT 19.5] Error: state too big (%d)", state);
       return true;
     }
 
     code = autocorrect_data[state];
-    LOG_DBG("[ANT-19.5] state: %d, code: %d, data: %s", state, code, autocorrect_data[state]);
-    log_array("AUTOCORRECT_DATA_ALL", autocorrect_data, AUTOCORRECT_MAX_LENGTH);
-    log_array("AUTOCORRECT_DATA", autocorrect_data, -1);
-    log_array("AUTOCORRECT_DATA_TAIL", autocorrect_data + state, ((sizeof(autocorrect_data) / sizeof(autocorrect_data[0])) - state));
+    LOG_DBG("[ANT 20] state: %d, code: %d, data: %d", state, code, autocorrect_data[state]);
+    log_array(20, "AUTOCORRECT_DATA_ALL", autocorrect_data, AUTOCORRECT_MAX_LENGTH);
+    log_array(20, "AUTOCORRECT_DATA", autocorrect_data, -1);
 
     if (code & (2 * (HIGH_BIT_MASK + 1))) { // A typo was found! Apply autocorrect.
       const uint32_t backspaces = (code & HIGH_BIT_MASK); // + !record->event.pressed;
-      LOG_DBG("[ANT-21] backspaces: %d", backspaces);
+      LOG_DBG("[ANT 21] backspaces: %d", backspaces);
       for (int i = 0; i < backspaces; ++i) {
-        LOG_DBG("[ANT-22] i: %d", i);
+        LOG_DBG("[ANT 22] i: %d", i);
         ZMK_EVENT_RAISE(
           new_zmk_keycode_state_changed(
             (struct zmk_keycode_state_changed){
@@ -226,7 +228,7 @@ bool process_autocorrect(uint32_t keycode, const zmk_event_t *record) {
 
       // send_string_P((char const *)(autocorrect_data + state + 1));
       for (int i = 0; i < (sizeof(autocorrect_data + state) / sizeof(autocorrect_data[0])); i++) {
-        LOG_DBG("[ANT-22.5] i: %d, state: %d, data: %d", i, state, (autocorrect_data + state + 1)[i]);
+        LOG_DBG("[ANT 22.5] i: %d, state: %d, data: %d", i, state, (autocorrect_data + state + 1)[i]);
         ZMK_EVENT_RAISE(
           new_zmk_keycode_state_changed(
             (struct zmk_keycode_state_changed){
@@ -248,19 +250,19 @@ bool process_autocorrect(uint32_t keycode, const zmk_event_t *record) {
       }
 
       if (keycode == 44) {
-        LOG_DBG("[ANT-23]");
+        LOG_DBG("[ANT 23]");
         typo_buffer[0]  = 44;
         typo_buffer_size = 1;
         return true;
       } else {
-        LOG_DBG("[ANT-24]");
+        LOG_DBG("[ANT 24]");
         typo_buffer_size = 0;
        return false;
       }
     }
   }
   return true;
-  LOG_DBG("[ANT-25]");
+  LOG_DBG("[ANT 25]");
 }
 
 ZMK_LISTENER(autocorrect, autocorrect_event_listener);
