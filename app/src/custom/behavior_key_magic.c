319c0313a4d60544bfdd968dfd93c2f7c5e74322
antlers <antlers@illucid.net>
2023-06-16 08:07:18 +0000

    time

--- app/src/custom/behavior_key_magic.c
+++ app/src/custom/behavior_key_magic.c
@@ -55,7 +55,7 @@ static int on_key_repeat_binding_pressed(struct zmk_behavior_binding *binding,
     void tap_keys (const char* str) {
       for (size_t i=0; i < strlen(str); i++) {
         tap_key(str[i]);
-        k_sleep(K_MSEC('40'));
+        k_sleep(K_USEC('100'));
         if (i != ( strlen(str) - 1 )); {
           data->current_keycode_pressed.timestamp = k_uptime_get();
           data->current_keycode_pressed.state = false;
